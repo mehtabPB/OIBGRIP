204b76517858e86aa0be051659b2fa19ca6cba08
Mehtab Alam <71491648+mehtabPB@users.noreply.github.com>
2025-09-25 09:36:23 +0530

    Update guess.java

--- guess.java
+++ guess.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 class guess{
-//Hello world 3
+//Hello world 4
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
@@ -44,5 +44,6 @@ class guess{
     }
 
 }
+
 
 
